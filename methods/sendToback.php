70035971c11c6759a4a8bcb9a03b700242135267
jean <jeanlucafp@gmail.com>
2017-10-18 20:17:24 -0200

    adicionado metodos post

---
+++ methods/sendToback.php
@@ -0,0 +1,27 @@
+<?php
+	$nome = $_POST["nome"];
+	$contato = $_POST["telefone"];
+	
+	$array = array("nome" => .$nome., "telefone" => .$contato);
+	
+	$json = json_encode($array);
+	
+	$ch = curl_init('https://neuraapp.com/leads');
+	
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+	
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+
+    'Content-Type: application/json',
+
+    'Content-Length: ' . strlen($json))
+
+);
+
+?>
+	
+	
