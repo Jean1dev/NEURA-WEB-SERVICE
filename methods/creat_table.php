d9dcb77160d1138a1393205bfd1147fb5dddae8b
Jean <jeanlucafp@gmail.com>
2018-01-17 22:35:19 -0200

    ...

--- methods/creat_table.php
+++ methods/creat_table.php
@@ -6,7 +6,7 @@
 	$create_table = "CREATE TABLE IF NOT EXISTS `turnos` (
 							`Id_Turnos` int(11) NOT NULL,
 							`Turnos` varchar(40) NOT NULL
-						) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;"
+						) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;";
 
 	$create = "CREATE TABLE minhatabela (
 				primeira_coluna int NOT NULL auto_increment,
