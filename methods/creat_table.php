35fd386d1cf433d7ff4f121fba5fd21733934c70
Jean <jeanlucafp@gmail.com>
2018-01-17 23:15:20 -0200

    ..

--- methods/creat_table.php
+++ methods/creat_table.php
@@ -1,31 +1,56 @@
 <?php 
 
 	include "db.php";
-	//mysql_select_db('neura_bd_teste');
 
-	$create_table = "CREATE TABLE IF NOT EXISTS `turnos` (
-							`Id_Turnos` int(11) NOT NULL,
-							`Turnos` varchar(40) NOT NULL
-						) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;";
+	$version = null ;
 
-	$create = "CREATE TABLE minhatabela (
-				primeira_coluna int NOT NULL auto_increment,
-				segunda_coluna varchar (20) NOT NULL,
-				terceira_coluna int NOT NULL,
-				PRIMARY KEY (primeira_coluna)
-				)";
-						
-	//$results = mysql_query($create) or die (mysql_error());
-	$stm = $scon->query($create);
+	$table = "CREATE TABLE IF NOT EXISTS `VersaoBD` (
+				  `idVersaoBD` INT NOT NULL AUTO_INCREMENT,
+				  `date` DATE NULL,
+				  PRIMARY KEY (`idVersaoBD`))
+				ENGINE = InnoDB;";					
+
+	$stm = $conn->query($table);
 	
-	if($stm->execute()){
-		echo "SUCCES";
-	}else{
-		echo "ERR";
+	$sql = 'SELECT idVersaoBD FROM VersaoBD ORDER BY idVersaoBD DESC LIMIT 1'
+	$exec_row = $conn->query($sql);
+	$retorno = $exec_row->fetch_row();
+	$retorno_row = $retorno[1];
+	
+	if($retorno[1] < This.getVersion()){
+		This.versaoBD()
 	}
-	
-	mysql_close();
+
 	$stm->close();
-	$con->close();
-		
+	$conn->close();
+	
+	function getVersion(){
+		$version = 2;
+		return $version;
+	}
+
+	function versaoBD(){
+		$versao_1 = "CREATE TABLE minhatabela (
+					primeira_coluna int NOT NULL auto_increment,
+					segunda_coluna varchar (20) NOT NULL,
+					terceira_coluna int NOT NULL,
+					PRIMARY KEY (primeira_coluna)
+					)";
+		$stm = $conn->query($versao_1);
+
+	}
+
+	function insertVersionBD(){
+		$data = date("Y-m-d H:i:s");
+		$sql_insert = 'INSERT INTO VersaoBD(date) VALUES(?)';
+		$stm = $conn->prepare($sql_insert);
+		$stm->bind_param("s", $data);
+
+		if($stm->execute()){
+			$stm->close();
+		}else{
+			echo 'Problema ao atualizar versão do banco';
+		}
+	}
+
 ?>
